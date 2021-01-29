5f0d10a7abc64b2e9e9dcd5f105be0551f0db8e2
Gabriel Manzini <manzini.gabriel@hotmail.com>
2021-01-29 01:54:27 -0300

    .dockerignore

---
+++ API2/Controller/ImovelController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace API2.Controller
+{
+    [Route("api/Imovel")]
+    [ApiController]
+    public class ImovelController : ControllerBase
+    {
+
+        // GET: ImovelController/Details/5
+        [HttpGet]
+        public IActionResult Details(double metrosQuadrados)
+        {
+
+            if (metrosQuadrados < 10 || metrosQuadrados > 10000)
+            {
+                return BadRequest("A quantidade de metros quadrados deve estar entre 10 e 10.000");               
+            }
+            else
+            {
+                using (HttpClient httpClient = new HttpClient() { BaseAddress = new Uri("https://api1-gmanzini.herokuapp.com/") })
+                {
+                    double result = double.Parse(httpClient.GetAsync("api/PrecificacaoController").Result.Content.ReadAsStringAsync().Result, CultureInfo.InvariantCulture);
+                    return Ok(result * metrosQuadrados);
+                }
+            }
+        }
+
+    
+    }
+}
---
+++ API2/Startup.cs
@@ -0,0 +1,48 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API2
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddMvc();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "API2 - Ferramenta para prover o preço do imóvel através da metragem", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json","API 2");
+            });
+        }
+    }
+}
